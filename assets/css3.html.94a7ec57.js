71b6e333d3675d7ff24e76a9dbaba4e899f99876
haohaidong <haohaidong@pingansec.com>
2024-05-17 15:44:49 +0800

    'deploy'

---
+++ assets/css3.html.94a7ec57.js
@@ -0,0 +1,72 @@
+import{_ as n,o as s,c as a,e as t}from"./app.b96cbd7c.js";var p="/myblog/images/frontEnd/img_16.png";const e={},c=t('<h1 id="\u5B9E\u73B0\u516D\u8FB9\u5F62\u8FDB\u5EA6\u6761\u73AF\u7ED5\u52A8\u753B\u6548\u679C" tabindex="-1"><a class="header-anchor" href="#\u5B9E\u73B0\u516D\u8FB9\u5F62\u8FDB\u5EA6\u6761\u73AF\u7ED5\u52A8\u753B\u6548\u679C" aria-hidden="true">#</a> \u5B9E\u73B0\u516D\u8FB9\u5F62\u8FDB\u5EA6\u6761\u73AF\u7ED5\u52A8\u753B\u6548\u679C</h1><p><img src="'+p+`" alt="\u56FE\u7247"></p><h3 id="clip-path\u5C5E\u6027\u5B9E\u73B0\u516D\u8FB9\u5F62" tabindex="-1"><a class="header-anchor" href="#clip-path\u5C5E\u6027\u5B9E\u73B0\u516D\u8FB9\u5F62" aria-hidden="true">#</a> clip-path\u5C5E\u6027\u5B9E\u73B0\u516D\u8FB9\u5F62</h3><div class="language-html ext-html line-numbers-mode"><pre class="language-html"><code><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>import-icon gradient-border<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>animation<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span><span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+    <span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>div</span> <span class="token attr-name">class</span><span class="token attr-value"><span class="token punctuation attr-equals">=</span><span class="token punctuation">&quot;</span>icon2<span class="token punctuation">&quot;</span></span><span class="token punctuation">&gt;</span></span>\u91CD\u8981<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;</span>br</span><span class="token punctuation">&gt;</span></span>\u63D0\u9192<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+<span class="token tag"><span class="token tag"><span class="token punctuation">&lt;/</span>div</span><span class="token punctuation">&gt;</span></span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><div class="language-css ext-css line-numbers-mode"><pre class="language-css"><code><span class="token comment">/** \u516D\u8FB9\u5F62\u80CC\u666F **/</span>
+<span class="token selector">.import-tip .import-icon</span> <span class="token punctuation">{</span>
+    <span class="token property">position</span><span class="token punctuation">:</span> relative<span class="token punctuation">;</span>
+    <span class="token property">box-sizing</span><span class="token punctuation">:</span> border-box<span class="token punctuation">;</span>
+    <span class="token property">display</span><span class="token punctuation">:</span> flex<span class="token punctuation">;</span>
+    <span class="token property">justify-content</span><span class="token punctuation">:</span> center<span class="token punctuation">;</span>
+    <span class="token property">align-items</span><span class="token punctuation">:</span> center<span class="token punctuation">;</span>
+    <span class="token property">margin</span><span class="token punctuation">:</span> 0 20px 0 40px<span class="token punctuation">;</span>
+    <span class="token property">background-color</span><span class="token punctuation">:</span> <span class="token function">rgba</span><span class="token punctuation">(</span>244<span class="token punctuation">,</span> 151<span class="token punctuation">,</span> 20<span class="token punctuation">,</span> 0.2<span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token property">font-size</span><span class="token punctuation">:</span> 36px<span class="token punctuation">;</span>
+    <span class="token property">font-weight</span><span class="token punctuation">:</span> bold<span class="token punctuation">;</span>
+    <span class="token property">color</span><span class="token punctuation">:</span> #ffffff<span class="token punctuation">;</span>
+    <span class="token property">width</span><span class="token punctuation">:</span> 169px<span class="token punctuation">;</span>
+    <span class="token property">height</span><span class="token punctuation">:</span> 196px<span class="token punctuation">;</span>
+    <span class="token property">clip-path</span><span class="token punctuation">:</span> <span class="token function">polygon</span><span class="token punctuation">(</span>
+            0 25%<span class="token punctuation">,</span>
+            50% 0<span class="token punctuation">,</span>
+            100% 25%<span class="token punctuation">,</span>
+            100% 75%<span class="token punctuation">,</span>
+            50% 100%<span class="token punctuation">,</span>
+            0 75%
+    <span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token comment">/** \u516D\u8FB9\u5F62\u9876\u5C42 **/</span>
+<span class="token selector">.import-icon .icon2</span> <span class="token punctuation">{</span>
+    <span class="token property">text-align</span><span class="token punctuation">:</span> center<span class="token punctuation">;</span>
+    <span class="token property">line-height</span><span class="token punctuation">:</span> 40px<span class="token punctuation">;</span>
+    <span class="token property">display</span><span class="token punctuation">:</span> flex<span class="token punctuation">;</span>
+    <span class="token property">align-items</span><span class="token punctuation">:</span> center<span class="token punctuation">;</span>
+    <span class="token property">justify-content</span><span class="token punctuation">:</span> center<span class="token punctuation">;</span>
+    <span class="token property">width</span><span class="token punctuation">:</span> 152px<span class="token punctuation">;</span>
+    <span class="token property">height</span><span class="token punctuation">:</span> 176px<span class="token punctuation">;</span>
+    <span class="token property">background-color</span><span class="token punctuation">:</span> #f49714<span class="token punctuation">;</span>
+    <span class="token property">clip-path</span><span class="token punctuation">:</span> <span class="token function">polygon</span><span class="token punctuation">(</span>
+            0 25%<span class="token punctuation">,</span>
+            50% 0<span class="token punctuation">,</span>
+            100% 25%<span class="token punctuation">,</span>
+            100% 75%<span class="token punctuation">,</span>
+            50% 100%<span class="token punctuation">,</span>
+            0 75%
+    <span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+
+<span class="token comment">/** \u4E09\u89D2\u5F62\u65CB\u8F6C\u5C42 **/</span>
+<span class="token selector">.import-icon .animation</span> <span class="token punctuation">{</span>
+    <span class="token property">width</span><span class="token punctuation">:</span> 100px<span class="token punctuation">;</span>
+    <span class="token property">height</span><span class="token punctuation">:</span> 100px<span class="token punctuation">;</span>
+    <span class="token property">background-image</span><span class="token punctuation">:</span> <span class="token function">linear-gradient</span><span class="token punctuation">(</span>-90deg<span class="token punctuation">,</span>
+    <span class="token function">rgba</span><span class="token punctuation">(</span>255<span class="token punctuation">,</span> 180<span class="token punctuation">,</span> 109<span class="token punctuation">,</span> 0.11<span class="token punctuation">)</span><span class="token punctuation">,</span>
+    <span class="token function">rgba</span><span class="token punctuation">(</span>255<span class="token punctuation">,</span> 123<span class="token punctuation">,</span> 0<span class="token punctuation">,</span> 0.41<span class="token punctuation">)</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token property">border-radius</span><span class="token punctuation">:</span> 50%<span class="token punctuation">;</span>
+    <span class="token property">clip-path</span><span class="token punctuation">:</span> <span class="token function">polygon</span><span class="token punctuation">(</span>50% 0%<span class="token punctuation">,</span> 0% 100%<span class="token punctuation">,</span> 100% 100%<span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token property">position</span><span class="token punctuation">:</span> absolute<span class="token punctuation">;</span>
+    <span class="token property">top</span><span class="token punctuation">:</span> 98px<span class="token punctuation">;</span>
+    <span class="token property">left</span><span class="token punctuation">:</span> 36px<span class="token punctuation">;</span>
+    <span class="token property">animation</span><span class="token punctuation">:</span> loading 1.5s linear infinite<span class="token punctuation">;</span>
+    <span class="token property">transform-origin</span><span class="token punctuation">:</span> 50px 0<span class="token punctuation">;</span>
+<span class="token punctuation">}</span>
+<span class="token comment">/** \u52A8\u753B **/</span>
+<span class="token atrule"><span class="token rule">@keyframes</span> loading</span> <span class="token punctuation">{</span>
+    <span class="token selector">0%</span> <span class="token punctuation">{</span>
+        <span class="token property">transform</span><span class="token punctuation">:</span> <span class="token function">rotate</span><span class="token punctuation">(</span>0deg<span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+    <span class="token selector">100%</span> <span class="token punctuation">{</span>
+        <span class="token property">transform</span><span class="token punctuation">:</span> <span class="token function">rotate</span><span class="token punctuation">(</span>360deg<span class="token punctuation">)</span><span class="token punctuation">;</span>
+    <span class="token punctuation">}</span>
+<span class="token punctuation">}</span>
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,5),o=[c];function i(l,u){return s(),a("div",null,o)}var k=n(e,[["render",i],["__file","css3.html.vue"]]);export{k as default};
